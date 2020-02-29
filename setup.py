71658d61098eba36bd1785e54481cda2512fcd3a
qraft technologies Q <koko8624@gmail.com>
2020-02-29 22:07:48 +0900

    feat!: BREAKING CAHNGE, PyPI에 v0.0.1 업로드

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="kiwoom_api_handler",
+    version="0.0.1",
+    description="a package for hanlding Kiwoom OPEN API+ ActiveX Control with python",
+    author="Donghyung Ko",
+    author_email="koko8624@gmail.com",
+    license="MIT",
+    url="https://github.com/donghyungko/kiwoom_api_handler.git",
+    download_url="https://github.com/DonghyungKo/kiwoom_api_handler/archive/master.zip",
+    install_requires=["numpy", "pandas", "pyqt5", "pyqt5-tools", "asyncio"],
+    packages=find_packages(exclude=[]),
+    keywords=["Kiwoom", "Kiwoom OPEN API+", "Kiwoom API", "키움증권"],
+    python_requires=">=3.6",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    package_data={},
+    zip_safe=False,
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+    ],
+)

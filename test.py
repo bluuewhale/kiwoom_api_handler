cc4c773b7de233ab14169a28a52532e24e57bd4a
qraft technologies Q <koko8624@gmail.com>
2020-02-29 20:45:46 +0900

    test: test.py 코드 수정

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ import pandas as pd
 from PyQt5.QtWidgets import QApplication
 
 from api import *
-
+from utility import *
 
 if __name__ == "__main__":
     pd.options.mode.chained_assignment = None
@@ -26,15 +26,15 @@ if __name__ == "__main__":
     today = dt.now().strftime("%Y%m%d")
     code = "005930"
 
-    print(feeder.getOPT10004(code))
-    print(feeder.getOPT10005(code))
-    print(feeder.getOPT10059(today, code))
-    print(feeder.getOPT10074(accNo, "20200101", today))
-    print(feeder.getOPT10075(accNo))
-    print(feeder.getOPTKWFID(["005930", "006630"]))
-    print(feeder.getOPW00001(accNo))
-    print(feeder.getOPW00004(accNo))
-    print(feeder.getOPW00007(today, accNo))
+    # print(feeder.getOPT10004(code))
+    # print(feeder.getOPT10005(code))
+    # print(feeder.getOPT10059(today, code))
+    # print(feeder.getOPT10074(accNo, "20200101", today))
+    # print(feeder.getOPT10075(accNo))
+    # print(feeder.getOPTKWFID(["005930", "006630"]))
+    # print(feeder.getOPW00001(accNo))
+    # print(feeder.getOPW00004(accNo))
+    # print(feeder.getOPW00007(today, accNo))
 
     orderSpecDict = executor.createOrderSpec(
         rqName="test",
